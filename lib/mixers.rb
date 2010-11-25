61043257febe1ab0ac1f2b8b8a90441471c56f58
7rans <transfire@gmail.com>
2010-11-25 09:39:54 -0500

    admin: must preset verison b/c of Ruby 1.8 issue

--- lib/mixers.rb
+++ lib/mixers.rb
@@ -2,10 +2,15 @@ module Mixers
   DIRECTORY = File.dirname(__FILE__)
 
   def self.version
-    @version ||= YAML.load(File.new(DIRECTORY + '/mixers/version.yml'))
+    @version ||= (
+      require 'yaml'
+      YAML.load(File.new(DIRECTORY + '/mixers/version.yml'))
+    )
   end
 
   def self.const_missing(name)
     version[name.to_s.downcase] || super(name)
   end
+
+  VERSION = version['version']
 end
